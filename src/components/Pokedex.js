e79fee608a83f70fad7ba8f1f5c0de4e1277ae76
MarcoAnyeloCazarez <anyelocaba7@gmail.com>
2022-04-11 01:36:09 -0500

    Agregue el map en el componente pokedex para traer los nombres de los pokemones

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const Pokedex = () => {
-    const userName = useSelector(state => state.userName)
+    const userName = useSelector(state => state.userName);
+    const [ pokemons, setPokemons ] = useState([])
+
+    useEffect(() => {
+        axios
+            .get('https://pokeapi.co/api/v2/pokedex/')
+            .then(answ => setPokemons(answ.data.results));
+    },[])
 
     return (
         <div>
             <h1> <b> Pokedex </b> </h1>
             <p>Welcome <b> { userName }</b>, here you can find your favorite pokemon</p>
+            {
+                pokemons.map(pokemon => (
+                    <li key={pokemon.name}>
+                        {pokemon.name}
+                    </li>
+                ))
+            }
         </div>
     );
 };
